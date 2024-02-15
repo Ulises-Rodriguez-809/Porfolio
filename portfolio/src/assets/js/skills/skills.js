5318c31f50bfdfcfefab128c55314b00349acdaa
Ulises <uliisesrodriguez809@gmail.com>
2024-02-15 09:39:43 -0300

    habilidades page listo (faltan fotos)

--- portfolio/src/assets/js/skills/skills.js
+++ portfolio/src/assets/js/skills/skills.js
@@ -4,27 +4,33 @@ export const SKILLSARRAY = [
         skills : [
             {
                 name : "HTML",
-                img : "/img/skillsImg/html.png"
+                img : "/img/skillsImg/html.png",
+                class : "htmlBG"
             },
             {
                 name : "CSS",
-                img : "/img/skillsImg/css.jpg"
+                img : "/img/skillsImg/css.jpg",
+                class : "cssBG"
             },
             {
                 name : "SASS",
-                img : "/img/skillsImg/sass.png"
+                img : "/img/skillsImg/sass.png",
+                class : "sassBG"
             },
             {
                 name : "Bootstrap",
-                img : "/img/skillsImg/bootstrap.png"
+                img : "/img/skillsImg/bootstrap.png",
+                class : "bootstrapBG"
             },
             {
                 name : "JavaScript",
-                img : "/img/skillsImg/js.png"
+                img : "/img/skillsImg/js.png",
+                class : "javaScriptBG"
             },
             {
                 name : "React JS",
-                img : "/img/skillsImg/reactjs.png"
+                img : "/img/skillsImg/reactjs.png",
+                class : "reactJSBG"
             },
         ]
     },
@@ -33,47 +39,58 @@ export const SKILLSARRAY = [
         skills : [
             {
                 name : "Node JS",
-                img : "/img/skillsImg/node.png"
+                img : "/img/skillsImg/node.png",
+                class : "nodeJSBG"
             },
             {
                 name : "Express",
-                img : "/img/skillsImg/express.png"
+                img : "/img/skillsImg/express.png",
+                class : "expressBG"
             },
             {
                 name : "express-handlebars",
-                img : "/img/skillsImg/reactjs.png"
+                img : "/img/skillsImg/reactjs.png",
+                class : "ehBG"
             },
             {
                 name : "Mongoose",
-                img : "/img/skillsImg/mongoose.png"
+                img : "/img/skillsImg/mongoose.png",
+                class : "mongooseBG"
             },
             {
                 name : "mongoose-paginate-v2",
-                img : "/img/skillsImg/npm.png"
+                img : "/img/skillsImg/npm.png",
+                class : "mpv2BG"
             },
             {
                 name : "connect-mongo",
-                img : "/img/skillsImg/npm.png"
+                img : "/img/skillsImg/npm.png",
+                class : "cmBG"
             },
             {
                 name : "bcrypt",
-                img : "/img/skillsImg/bcrypt.png"
+                img : "/img/skillsImg/bcrypt.png",
+                class : "bcryptBG"
             },
             {
                 name : "cookie-parser",
-                img : "/img/skillsImg/cookieparser.png"
+                img : "/img/skillsImg/cookieparser.png",
+                class : "cpBG"
             },
             {
                 name : "passport",
-                img : "/img/skillsImg/passport.png"
+                img : "/img/skillsImg/passport.png",
+                class : "paspportBG"
             },
             {
                 name : "dotenv",
-                img : "/img/skillsImg/dotenv.png"
+                img : "/img/skillsImg/dotenv.png",
+                class : "dotenvBG"
             },
             {
                 name : "socket.io",
-                img : "/img/skillsImg/socket.png"
+                img : "/img/skillsImg/socket.png",
+                class : "soketBG"
             },
         ]
     },
@@ -82,15 +99,18 @@ export const SKILLSARRAY = [
         skills : [
             {
                 name : "Mongo DB Atlas",
-                img : "/img/skillsImg/mongo.png"
+                img : "/img/skillsImg/mongo.png",
+                class : "maBG"
             },
             {
                 name : "SQL",
-                img : "/img/skillsImg/sql.png"
+                img : "/img/skillsImg/sql.png",
+                class : "sqlBG"
             },
             {
                 name : "Firebase",
-                img : "/img/skillsImg/firebase.png"
+                img : "/img/skillsImg/firebase.png",
+                class : "firebaseBG"
             },
         ]
     },
@@ -99,7 +119,8 @@ export const SKILLSARRAY = [
         skills : [
             {
                 name : "Python",
-                img : "/img/skillsImg/python.jpg"
+                img : "/img/skillsImg/python.jpg",
+                class : "pythonBG"
             },
         ]
     }
